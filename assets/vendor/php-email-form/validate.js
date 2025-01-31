508434a8ad8c974032636db844bb29211d409821
Roumak77 <roumak.github@gmail.com>
2025-01-31 16:17:27 +0530

    Prototype

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,9 +1,4 @@
-/**
-* PHP Email Form Validation - v3.9
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
-(function () {
+/*(function () {
   "use strict";
 
   let forms = document.querySelectorAll('.php-email-form');
@@ -82,4 +77,4 @@
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
-})();
+})();*/
